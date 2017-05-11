f6a8ddbafeac25df082eaa76da57d3b955e6cf8d
jacquesbonet <jacques.bonet@gmail.com>
2017-05-11 15:00:40 +0200

    Simplification

--- chapter-06 Props, State, Component Tree/color-organizer/src/components/Color.js
+++ chapter-06 Props, State, Component Tree/color-organizer/src/components/Color.js
@@ -3,23 +3,43 @@ import PropTypes from 'prop-types'
 import StarRating from './StarRating'
 import SmartTitle from './SmartTitle'
 
-const Color = ({ title, color, rating=0, onRemove=f=>f, onRate=f=>f, onChangeTitle=f=>f}) =>
-    <section>
-        <SmartTitle title={title} onChangeTitle={onChangeTitle}/>
-        <button onClick={onRemove}>X</button>
-        <div>
-            <StarRating starsSelected={rating} onRate={onRate}/>
-        </div>
-    </section>
+
+export default class Color extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    rateColor(rating) {
+        this.props.onUpdate( this.props.id, rating)
+    }
+
+    changeTitle(title) {
+        this.props.onUpdate( this.props.id, title)
+    }
+
+    render() {
+        const {title, rating, onUpdate} = this.props
+
+        return (
+            <section>
+                <SmartTitle title={title} onChangeTitle={title => this.changeTitle(title)}/>
+                <button onClick={onRemove}>X</button>
+                <div>
+                    <StarRating starsSelected={rating} onRate={ rate => this.rateColor(rate)}/>
+                </div>
+            </section>
+        )
+    }
+}
 
 Color.propTypes = {
     title: PropTypes.string.isRequired,
-    color: PropTypes.string.isRequired,
     rating: PropTypes.number,
     onRemove: PropTypes.func,
-    onRate: PropTypes.func
+    onUpdate: PropTypes.func
+
 }
 
 export default Color
-
 
